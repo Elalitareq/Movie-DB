9d62f516394e3aca36379b571d67f28473b19609
Tareq El-Ali <elalitareq@gmail.com>
2022-12-24 11:20:52 +0200

    step 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 9797;
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 app.get("/:par1?/:par2?", (req, res) => {
   let par1 = req.params.par1;
   let par2 = req.params.par2;
@@ -23,19 +29,26 @@ app.get("/:par1?/:par2?", (req, res) => {
       ? res.send({ status: 200, message: par1 })
       : res.send({ status: 200, message: `${par1}, ${par2}` });
   } else if (par1 == "search") {
-    if(req.query.s == "" || req.query.s == undefined)
-    {
-        let search = { status: 500, message: `You have to provide a search` };
-        res.send(search);
-        res.status(500).send()
+    if (req.query.s == "" || req.query.s == undefined) {
+      let search = { status: 500, message: `You have to provide a search` };
+      res.send(search);
+      res.status(500).send();
+    } else {
+      let search = { status: 200, message: `OK`, data: req.query.s };
+      res.send(search);
     }
-    else{
-        let search = { status: 200, message:`OK`, data: req.query.s };
-        res.send(search);
-
-    }
-  } else{
-    res.send(`${par1} is not a directory`)
+  } else if (par1 == "movies") {
+    par2 == "create"
+      ? res.send("create")
+      : par2 == "read"
+      ? res.send({status:200,data:movies})
+      : par2 == "update"
+      ? res.send("update")
+      : par2 == "delete"
+      ? res.send("delete")
+      : res.send("choose a directory");
+  } else {
+    res.send(`${par1} is not a directory`);
   }
 });
 app.listen(port, () => {
