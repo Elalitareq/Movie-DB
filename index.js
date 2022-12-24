dc19e93971df5e43dcef99ded965fd77c400c4e9
Tareq El-Ali <elalitareq@gmail.com>
2022-12-24 08:41:38 +0200

    Create an express simple API

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 9797
+const express = require("express");
+const app = express();
+const port = 9797;
 
-app.get('/', (req, res) => {
-  res.send('OK')
-})
-
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+app.get("/test", (req, res) => {
+  const response = {
+    status: 200,
+    message: "ok",
+  };
+  res.send(response);
+});
+app.get("/time", (req, res) => {
+  let date = new Date();
+  let time = `${date.getHours()}:${date.getMinutes()}}`;
+  let response = { status: 200, message: time };
+  res.send(response);
+});
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
-})
+  console.log(`listening at http://localhost:${port}`);
+});
