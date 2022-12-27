72cc65d739e3d77624bc5ab9323a9da567a3284c
Tareq El-Ali <elalitareq@gmail.com>
2022-12-27 22:20:08 +0200

    better put

--- routes/movies.js
+++ routes/movies.js
@@ -98,44 +98,38 @@ movieRouter.delete("/:id", async (req, res) => {
 movieRouter.put("/:par3?", async (req, res) => {
   let par3 = req.params.par3;
   if (isNaN(par3)) {
-    res.status(404).send({
-      status: 404,
+    return res.status(400).send({
+      status: 400,
       error: true,
-      message: `please select an ID number`,
+      message: `please select a valid ID number`,
     });
-  } else {
-    var title = req.query.title;
-    var year = req.query.year;
-    var rating = req.query.rating;
-    const filter = { _id: par3 };
-    var update = {};
+  }
 
-    title !== undefined ? (update.title = title) : update;
-    year !== undefined && year.toString().length == 4 && !isNaN(year)
-      ? (update.year = parseInt(year))
-      : update;
+  const filter = { _id: par3 };
+  let update = {};
+  const { title, year, rating } = req.query;
 
-    rating !== undefined && rating !== NaN && rating >= 0 && rating <= 10
-      ? (update.rating = parseFloat(rating))
-      : update;
-    console.log(update);
-    try {
-      await movieList.countDocuments(filter); // 0
+  if (title) update.title = title;
+  if (year && year.toString().length === 4 && !isNaN(year)) update.year = parseInt(year);
+  if (rating && !isNaN(rating) && rating >= 0 && rating <= 10) update.rating = parseFloat(rating);
 
-      let movie = await movieList.findOneAndUpdate(filter, update, {
-        new: true,
+  try {
+    let movie = await movieList.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+
+    if (!movie) {
+      return res.status(404).send({
+        status: 404,
+        error: true,
+        message: `ID:${par3} not found `,
       });
-
-      movie !== null
-        ? res.send({ status: 200, data: movie })
-        : res.status(404).send({
-            status: 404,
-            error: true,
-            message: `ID:${par3} not found `,
-          });
-    } catch (err) {
-      res.status(500).send(err);
     }
+
+    res.send({ status: 200, data: movie });
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
+
 module.exports = movieRouter;
